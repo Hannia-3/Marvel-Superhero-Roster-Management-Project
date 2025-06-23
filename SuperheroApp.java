b328dd04d586cb6d328d28fd4ef97d605cfdfd4b
Hannia-3 <Hannia.EscPM@gmail.com>
2025-06-22 22:00:15 -0500

    Update SuperheroApp.java

--- SuperheroApp.java
+++ SuperheroApp.java
@@ -3,31 +3,31 @@ import java.util.Scanner;
 public class SuperheroApp {
 
     public static void main(String[] args) {
-        // Declare and Initialize Arrays
-        String[] heroNames = {"Spider-Man", "Iron Man", "Black Widow"};
-        String[] abilities = {"Web-slinging", "Flying and high-tech armor", "Stealth and combat"};
-        int[] powerLevels = {85, 90, 80};
+        
+        String[] heroNames = {"Spider-Man", "Quicksilver", "Captain America"};
+        String[] abilities = {"Web-slinging", "Superhuman speed", "Combat"};
+        int[] powerLevels = {85, 90, 75};
 
-        // Loop to Display Each Hero's Details
+        
         System.out.println("--- Hero Details ---");
         for (int i = 0; i < heroNames.length; i++) {
             System.out.println("Name: " + heroNames[i] + ", Ability: " + abilities[i] + ", Power Level: " + powerLevels[i]);
         }
         System.out.println();
 
-        // Search Function (searchHero)
+        
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a hero name to search: ");
         String targetHero = scanner.nextLine();
         searchHero(heroNames, abilities, powerLevels, targetHero);
         System.out.println();
 
-        // Calculate Average Power (calculateAveragePower)
+        
         double averagePower = calculateAveragePower(powerLevels);
         System.out.println("Average Power Level: " + averagePower);
         System.out.println();
 
-        // Superhero Class and Object Array
+        
         Superhero[] superheroes = new Superhero[heroNames.length];
         for (int i = 0; i < heroNames.length; i++) {
             superheroes[i] = new Superhero(heroNames[i], abilities[i], powerLevels[i]);
@@ -41,7 +41,7 @@ public class SuperheroApp {
         scanner.close();
     }
 
-    // searchHero method
+    
     public static void searchHero(String[] names, String[] abilities, int[] powerLevels, String target) {
         boolean found = false;
         for (int i = 0; i < names.length; i++) {
@@ -57,7 +57,7 @@ public class SuperheroApp {
         }
     }
 
-    // calculateAveragePower method
+    
     public static double calculateAveragePower(int[] powerLevels) {
         int sum = 0;
         for (int power : powerLevels) {
@@ -67,20 +67,20 @@ public class SuperheroApp {
     }
 }
 
-// Superhero Class
+
 class Superhero {
     String name;
     String ability;
     int powerLevel;
 
-    // Constructor
+    
     public Superhero(String name, String ability, int powerLevel) {
         this.name = name;
         this.ability = ability;
         this.powerLevel = powerLevel;
     }
 
-    // displayHero method
+    
     public void displayHero() {
         System.out.println("Name: " + name + ", Ability: " + ability + ", Power Level: " + powerLevel);
     }
