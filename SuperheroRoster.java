a0ff6d277634ecfae15a620125da88ef8cea418a
Hannia-3 <Hannia.EscPM@gmail.com>
2025-06-22 21:23:22 -0500

    Update SuperheroRoster.java

--- SuperheroRoster.java
+++ SuperheroRoster.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class SuperheroRoster {
 
     public static void main(String[] args) {
-
         String[] heroNames = {"Spiderman", "Captain America", "Quicksilver"};
         String[] abilities = {"Web-slinging", "Stamina", "Superhuman speed"};
         int[] powerLevels = {80, 75, 90}
@@ -15,11 +14,49 @@ public class SuperheroRoster {
             System.out.println("Power Level: " + powerLevels[i]);
         }
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter a superhero name to search: ");
-        String name = scanner.nextLine();
-        boolean heroFound = searchHero (heroNames, abilities, poweLevels, Name);
+        String targetHero = "Spiderman";
+        System.out.println("\nSearching for:" + targetHero);
+        searchHero(heroNames, abilities, powerLevels, targetHero);
 
+        targetHero = "Captain America";
+        System.out.println("\nSearching for:" + targetHero);
+        searchHero(heroNames, abilities, powerLevels, targetHero);
+
+        targetHero = "Quicksilver";
+        System.out.println("\nSearching for:" + targetHero);
+        searchHero(heroNames, abilities, powerLevels, targetHero);
+
+        double averagePower = calculateAveragePower(powerLevels);
+        System.out.println("\nAverage Power Level: " + String.format("%.2f", averagePower));
+    }
+
+    public static void searchHero(String[] names, String[] abilities, int[] powerLevels, String target) {
+        boolean found = false;
+        for (int i = 0; i < heroNames.length; i++) {
+            if (names[i].equalsIgnoreCase(target)) {
+                System.out.println("Hero Found: " + names[i] + ", Ability: " + abilities[i] + ", Power Level: " + powerlevels[i]);
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            System.out.println("Hero '" + target + "' not found.");
+        }
+    }
+
+    public static double calculateAveragePower(int[] powerLevels) {
+        if (powerLevels.length == 0) {
+            return 0.0;
+        }
+        int sum = 0;
+        for (int power : powerLevels) {
+            sum += power;
+        }
+        return (double) sum / powerLevels.length;
+    }
+}
+        
+        
         
             
             
